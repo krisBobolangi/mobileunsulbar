36ab5530a69377b7ca8defd9a0c4fa4e6f95cfb3
krisBobolangi <99783828+krisBobolangi@users.noreply.github.com>
2022-04-21 18:01:15 -0700

    Add files via upload

---
+++ KrisBobolangi'_D0219511_TugasMobile/index.php
@@ -0,0 +1,20 @@
+<! DOCTYPE html>
+<html>
+<head>
+     <table>Pendaftaran Siswa Baru | SMAN Coding</table>
+<head>
+
+<body>
+   <header> 
+       <h3>Pendaftaran Siswa Baru</h3>
+       <h1>SMAN Coding</h1>
+</header>
+<h4>Menu</h4>
+<nav>
+    <ul>
+        <li><a href="from-daftar .php">Daftar Baru</a></li>
+        <li><a href="list-siswa . php">Pendaftar</a></li>
+    </ul>
+</nav>
+</body>
+</htm>
